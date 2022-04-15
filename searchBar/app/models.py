0e3b5fe3039392e3e16a0e442720481f7587b8bd
ConceptaNjolima <njolimac@berea.edu>
2022-04-14 23:47:45 -0400

    Edit return for the __repr__ function in models.py file

--- searchBar/app/models.py
+++ searchBar/app/models.py
@@ -2,13 +2,15 @@ from app import db
 
 
 class Farmer(db.Model):
-    __tablename__= 'Farmer'
+    __tablename__ = 'Farmer'
     farmerID = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(50), nullable=False)
     lastName = db.Column(db.String(50), nullable=False)
     county = db.Column(db.String(50), nullable=False)
+
     def __repr__(self):
-        return f'Farmer {self.farmerID}'
+        return " {} {}".format(self.farmerID, self.firstName)
+
 
 resultlist = Farmer.query.all()
 print(resultlist)
