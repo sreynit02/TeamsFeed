b830460f36358c93780d88fb6c042625c47c1754
kumbula <kumbulat@berea.edu>
2022-04-16 19:43:54 -0400

    Farmer search

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -2,16 +2,18 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@127.0.0.1:3307/databasename'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://User1:Berea#CSC330@127.0.0.1:3306/feedingky'
 app.config['SQLALCHEMY_ECHO'] = True;
 db = SQLAlchemy(app)
 
+from models import Farmer
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
 
-@app.route("/search")
+@app.route("/search", methods = ['GET'])
 # Function call to render search page
 def renderSearchPage():
-    return render_template("search.html")
+    farmers = Farmer.query.all()
+    return render_template("search.html", farmers=farmers)
