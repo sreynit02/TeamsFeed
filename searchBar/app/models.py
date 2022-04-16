bde19de9ffab9e799ce4542e25e9d28cfeb3dd84
sreynitkhatt <khatts@berea.edu>
2022-04-16 19:55:49 -0400

    added the rest of the table, still need to figure out how to add foreign key constrant

--- searchBar/app/models.py
+++ searchBar/app/models.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from sqlalchemy import UniqueConstraint
+from sqlalchemy.dialects.mysql import TINYINT
 from app import db
 
 class Farmer(db.Model):
@@ -68,13 +70,75 @@ class Funder(db.Model):
      def __repr__(self):
         return " {} {}".format(self.funderID,  self.funderLink)
 
+class Grant(db.Model):
+    __tablename__='Grant'
+    grantID = db.Column(db.Integer, nullable=False, primary_key=True)
+    funderID = db.Column(db.Integer) #need to make this foreign key
+    totalAward = db.Column(db.Float, nullable=False)
+    totalBudgetUsed = db.Column(db.Float, default='NULL')
+    startDate = db.Column(db.Date, default='NULL')
+    endDate = db.Column(db.Date, default='NULL')
+    restrictions = db.Column(db.String(1000), default='NULL')
+    reportingRequirement = db.Column(db.String(1000), default='NULL')
+    recordRequirement = db.Column(db.String(1000), default='NULL')
+    closed = db.Column(default='NULL') 
+    programName = db.Column(db.String(50), default='NULL')
+    grantType = db.Column(db.String(45), default='grant_type')
+    UniqueConstraint(grantID)
+    TINYINT(closed)
+    def __repr__(self):
+        return " {} {}".format(self.grantID,  self.totalAward)
+
+class PurchasedProduce(db.Model):
+    __tablename__='PurchasedProduce'
+    pfID = db.Column(db.Integer, nullable=False, primary_key=True)
+    foodID = db.Column(db.Integer, default='NULL') #need to make this foreign key
+    quantity = db.Column(db.Integer, nullable=False)
+    unitPrice = db.Column(db.Float, nullable=False)
+    unit = db.Column(db.String(20), nullable=False)
+    UniqueConstraint(pfID)
+
+    def __repr__(self):
+        return " {} {}".format(self.pfID,  self.quantity)
+
+class Invoices(db.Model):
+    __tablename__='Invoices'
+    invoiceNo = db.Column(db.Integer, nullable=False, primary_key=True)
+    invoiceType = db.Column(db.String(50), nullable=False)
+    outsideInvoice = db.Column(db.String(50), nullable=False)
+    dateReceived = db.Column(db.Date, nullable=False)
+    datePaid = db.Column(db.Date, nullable=False)
+    totalPound = db.Column(db.Float, nullable=False)
+    totalCost = db.Column(db.Float, nullable=False)
+    checkNo = db.Column(db.Integer, default='NULL')
+    deliveryFee = db.Column(db.Float, default='NULL')
+    buyFee = db.Column(db.Float, default='NULL')
+    purchaseOrder = db.Column(db.String(255), default='NULL')
+    pfID = db.Column(db.Integer, default='NULL') #foreign Key
+    grantID = db.Column(db.Integer, default='NULL') #foreign Key
+    farmerID = db.Column(db.Integer, default='NULL') #foreign Key
+    auctionID = db.Column(db.Integer, default='NULL') #foreign Key
+    foodBankID = db.Column(db.Integer, default='NULL') #foreign Key
+    UniqueConstraint(invoiceNo)
+
+    def __repr__(self):
+        return " {} {}".format(self.invoiceNo,  self.totalCost)
+
+
+
 # resultFarmer = Farmer.query.all()
 # resultFood = Food.query.all()
 # resultAuction = Auction.query.all()
 # resultFoodBank = FoodBank.query.all()
-resultFunder = Funder.query.all()
+# resultFunder = Funder.query.all()
+# resultGrant = Grant.query.all()
+# resultpurchasedProduce = PurchasedProduce.query.all()
+resultInvoice = Invoices.query.all()
+#print(resultpurchasedProduce)
+print(resultInvoice)
+#print(resultFunder)
 #print (resultFarmer)
 #print (resultFood)
 #print (resultAuction)
 #print (resultFoodBank)
-print (resultFunder)
+#print (resultGrant)
