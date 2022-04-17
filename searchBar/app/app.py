f2d21ee59f5aa63bd9091a2ffcec8d92e61719b0
ConceptaNjolima <njolimac@berea.edu>
2022-04-17 19:47:54 -0400

    import model inside the function to eliminate circular imports error

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template
+# from exts import db
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@host/database'
-app.config['SQLALCHEMY_ECHO'] = True;
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user1:feedingky#DBMS@127.0.0.1:3306/feedingky'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-from models import Farmer
+# def register_extensions(app):
+#     db.init_app(app)
+
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
 
-@app.route("/search", methods = ['GET'])
+@app.route("/search", methods=['GET'])
 # Function call to render search page
 def renderSearchPage():
+    from models import Farmer
     farmers = Farmer.query.all()
     return render_template("search.html", farmers=farmers)
