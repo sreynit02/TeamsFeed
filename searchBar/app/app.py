d7efb7613573289a6f833e703d8989b695616fe5
ConceptaNjolima <njolimac@berea.edu>
2022-04-17 21:04:38 -0400

    Add query to count the total number of counties where farmers are from

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, func
+
+
+engine = create_engine(
+    "mysql+pymysql://user1:feedingky#DBMS@127.0.0.1:3306/feedingky",
+)
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user1:feedingky#DBMS@127.0.0.1:3306/feedingky'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@127.0.0.1:3306/feedingky'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -16,5 +23,9 @@ def hello_world():
 # Function call to render search page
 def renderSearchPage():
     from models import Farmer
-    farmers = Farmer.query.all()
-    return render_template("search.html", farmers=farmers)
+    # farmers = Farmer.query.all()
+    counties = Farmer.query.with_entities(Farmer.county, func.count(
+        Farmer.county)).group_by(Farmer.county).all()
+    # query = db.select(Farmer.county).count()
+    # result = engine.execute(query).fetchall()
+    return render_template("search.html", counties=counties)
