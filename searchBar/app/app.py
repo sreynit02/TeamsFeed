7630cf9a78f44a37295084ec8c6b617be225ca7f
ConceptaNjolima <njolimac@berea.edu>
2022-04-17 21:17:21 -0400

    Clean out comments from app.py file

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -9,7 +9,7 @@ engine = create_engine(
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@127.0.0.1:3306/feedingky'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user1:feedingky#DBMS@127.0.0.1:3306/feedingky'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -20,12 +20,9 @@ def hello_world():
 
 
 @app.route("/search", methods=['GET'])
-# Function call to render search page
+# Function call to render search page for farmer query
 def renderSearchPage():
     from models import Farmer
-    # farmers = Farmer.query.all()
     counties = Farmer.query.with_entities(Farmer.county, func.count(
         Farmer.county)).group_by(Farmer.county).all()
-    # query = db.select(Farmer.county).count()
-    # result = engine.execute(query).fetchall()
     return render_template("search.html", counties=counties)
