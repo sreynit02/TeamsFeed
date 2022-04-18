f8f5cc61ad3cbb89a1d649a768027a24e3a6ef2e
ConceptaNjolima <njolimac@berea.edu>
2022-04-18 17:31:14 -0400

    Add individual routing that checks for the option selected and adds the value of option to route url

--- searchBar/app/static/index.js
+++ searchBar/app/static/index.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
     //When the search button is clicked 
     $("#SearchButton").click(function() {
-        // get text of the selected query from the dropdown
-        let selectText = $('#searchDropdown :selected').text();
-        // route to search page when search button is clicked
-        window.location.href = '/search';
-        console.log(selectText);
+        // get value of the selected query from the dropdown
+        let selectValue = $('#searchDropdown').val();
+        // route to search page/optionselected when search button is clicked
+        window.location.href = '/search/'+selectValue;
        
       })
 });
