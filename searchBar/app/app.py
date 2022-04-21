72c9737fef3b479a19048328eedb49f844ba24a4
ConceptaNjolima <njolimac@berea.edu>
2022-04-20 22:35:04 -0400

    Add javascript to get searchResults from html to Javascript

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, func
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user:Superliminal2019!@127.0.0.1:3306/feedky"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user1:feedingky#DBMS@127.0.0.1:3306/feedingky"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -14,43 +14,42 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route("/search/<value>", methods=['GET'])
+@app.route("/search/<value>", methods=['GET', 'POST'])
 # Function call to render search results based on the option selected from the dropdown menu
-
 def renderSearchPage(value):
-    if value=="1":
+    if value == "1":
         # funding spent query
         return render_template("selectError.html")
-    elif value=="2":
+    elif value == "2":
         #  types of produce query
         return render_template("selectError.html")
-    elif value=="3":
+    elif value == "3":
         # pounds distributed
         return render_template("selectError.html")
-    elif value=="4":
+    elif value == "4":
         # meals supplemented
         return render_template("selectError.html")
-    elif value=="5":
+    elif value == "5":
         # farmers that participate in program
         from models import Farmer
         counties = Farmer.query.with_entities(Farmer.county, func.count(
             Farmer.county)).group_by(Farmer.county).all()
-        return render_template("search.html", searchResults=counties)
-    elif value=="5":
+        variableType = type(counties)
+        return render_template("search.html", searchResults=counties, variableType=variableType)
+    elif value == "5":
         # farmers that participate in program
         return render_template("selectError.html")
-    elif value=="6":
+    elif value == "6":
         # farmers that participate in program
         return render_template("selectError.html")
-    elif value=="7":
+    elif value == "7":
         # farmers that participate in program
         return render_template("selectError.html")
-    elif value=="8":
+    elif value == "8":
         # farmers that participate in program
         return render_template("selectError.html")
-    elif value=="5":
+    elif value == "5":
         # farmers that participate in program
         return render_template("selectError.html")
     else:
         return render_template("selectError.html")
-
---
+++ searchBar/app/static/search.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    console.log("here")
+    // var searchResults=$('#results').data();
+    // console.log(searchResults)
+    // let results2 = $("meta[property='results']").attr('content');
+    // console.log(results2)
+    // google.charts.load('current', {'packages':['corechart']});
+    //   google.charts.setOnLoadCallback(drawChart);
+      let results2 = $("meta[property='results']").attr('content');
+      console.log(type(results2));
+      console.log(results2[3])
+      // listLength=results2.length;
+      // resultArray=[]
+      // for (let i=0;i<listLength;i++){
+      //     data=[]
+      //     data.push(results2[0][0])
+      //     data.push(results2[0][1])
+      //     resultArray.push(data)
+    
+    
+    //   console.log(result);
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(results2);
+
+        // var data = google.visualization.arrayToDataTable([
+        //   ['Task', 'Hours per Day'],
+        //   [,11],
+        //   ['Eat',      2],
+        //   ['Commute',  2],
+        //   ['Watch TV', 2],
+        //   ['Sleep',    7]
+        // ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+  
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+
+});
