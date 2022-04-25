6be858de028af3a13129eee1b3fb90e52088e40e
ConceptaNjolima <njolimac@berea.edu>
2022-04-24 21:52:48 -0400

    Add customSearchDisplay branch

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -3,18 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from numpy import average, full
 from sqlalchemy import create_engine, func
 
-#create flask application and import database (be sure to put in your username/password/name of database)
+# create flask application and import database (be sure to put in your username/password/name of database)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://user:password@127.0.0.1:3306/feedingky"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-#default function that calls main page
+# default function that calls main page
+
+
 @app.route("/")
 def render_homepage():
     return render_template("index.html")
 
-#run the route that is connected to the search page
+# run the route that is connected to the search page
+
+
 @app.route("/search/<value>", methods=['GET', 'POST'])
 # Function call to render search results based on the option selected from the dropdown menu
 def renderSearchPage(value):
@@ -79,8 +83,10 @@ def renderSearchPage(value):
         from models import Invoices
         from models import Farmer
         # Farmers who received more than $10000
-        farmerGrant = Invoices.query.with_entities(Invoices.farmerID).filter(Invoices.totalCost > 10000).distinct()
-        farmerName = Farmer.query.with_entities(Farmer.firstName).filter(Farmer.farmerID == farmerGrant).distinct()
-        return render_template("search.html", farmerName = farmerName)
+        farmerGrant = Invoices.query.with_entities(
+            Invoices.farmerID).filter(Invoices.totalCost > 10000).distinct()
+        farmerName = Farmer.query.with_entities(Farmer.firstName).filter(
+            Farmer.farmerID == farmerGrant).distinct()
+        return render_template("search.html", farmerName=farmerName)
     else:
         return render_template("selectError.html")
