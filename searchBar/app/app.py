37e4d5c3ed0858e23a10f64974bcce2876f76d6e
kumbula <kumbulat@berea.edu>
2022-04-28 14:02:03 -0400

    Decimal first query

--- searchBar/app/app.py
+++ searchBar/app/app.py
@@ -1,8 +1,10 @@
+from decimal import ROUND_UP
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from numpy import average, full
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
+from decimal import Decimal
 
 # create flask application and import database (be sure to put in your username/password/name of database)
 app = Flask(__name__)
@@ -30,6 +32,8 @@ def renderSearchPage(value):
         from models import Invoices
         totalCost = Invoices.query.with_entities(
             func.sum(Invoices.totalCost)).all()[0][0]
+        totalCost = Decimal(totalCost)
+        totalCost = round(totalCost, 2)
         # query all invoices to show in table
         invoices = Invoices.query.all()
 
@@ -71,6 +75,9 @@ def renderSearchPage(value):
         from models import Food
         pounds = PurchasedProduce.query.with_entities(
             func.sum(PurchasedProduce.quantity)).all()
+      
+        #pounds = Decimal(pounds)
+        # pounds = round(pounds)
         food = Food.query.join(PurchasedProduce, Food.foodID == PurchasedProduce.foodID).add_columns(Food.foodName, func.sum(PurchasedProduce.quantity)).filter(Food.foodID == PurchasedProduce.foodID).group_by(Food.foodName).all()
         #foodQuantities=PurchasedProduce.query.with_entities(PurchasedProduce.foodID, func.sum(PurchasedProduce.quantity)).group_by(PurchasedProduce.foodID).all()
         foodQuntityList = []
